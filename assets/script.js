eb0999dec5d7a678040934a07ee8f5acfe06cba6
Dave Collison <dave@davecollison.dev>
2023-11-28 18:15:56 +0000

    Refactor selectTopic()

--- assets/script.js
+++ assets/script.js
@@ -8,16 +8,10 @@ function listTopics () {
 }
 
 function selectTopic () {
-  if (randomTopic === 'HTML') {
-    console.log("Let's study HTML!");
-  } else if (randomTopic === 'CSS') {
-    console.log("Let's study CSS!");
-  } else if (randomTopic === 'Git') {
-    console.log("Let's study Git!");
-  } else if (randomTopic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
+  if (topics.includes(randomTopic)) {
+    console.log(`Let's study ${randomTopic}!`);
   } else {
-    console.log('Please try again!');
+    console.log(`${randomTopic} is not a topic we learned through Prework. Please try again!`);
   }
 }
 

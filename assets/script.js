37ca15fa35be3ac411a2dabc0fa498b28187a3aa
Dave Collison <dave@davecollison.dev>
2023-11-28 19:50:03 +0000

    Add error checking to the two array functions

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 // var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-function list (array) {
+function list (array = []) {
+  if (!array.length) return console.log('No items in array');
+
   for (var item = 0; item < array.length; item++) {
     console.log(array[item]);
   }
 }
 
-function selectRandomFrom (array, message = 'Selected') {
+function selectRandomFrom (array = [], message = 'Selected') {
+  if (!array.length) return console.log('No items in array');
+
   const randomSelection = array[Math.floor(Math.random() * array.length)];
   console.log(`${message} ${randomSelection}.`);
 }

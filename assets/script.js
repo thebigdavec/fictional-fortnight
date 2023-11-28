ae7218426dbd2258c9b0b398bf6fe15722770ad9
Dave Collison <dave@davecollison.dev>
2023-11-28 19:48:18 +0000

    refactor listTopics function to generic list(array) function

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 // var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-function listTopics () {
-  for (var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function list (array) {
+  for (var item = 0; item < array.length; item++) {
+    console.log(array[item]);
   }
 }
 
 function selectRandomFrom (array, message = 'Selected') {
   const randomSelection = array[Math.floor(Math.random() * array.length)];
-  console.log(`${message} ${randomSelection}.`)``;
+  console.log(`${message} ${randomSelection}.`);
 }
 
 console.log('Here are the topics we learned through Prework:');
-listTopics();
+list(topics);
 console.log('Which topic should we study first?');
 selectRandomFrom(topics, "Let's study");
